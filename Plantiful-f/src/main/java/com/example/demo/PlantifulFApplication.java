2d9bf36dc4f1956a30f2030b66ee9eb5887b76b5
hennie-yun <hennie9401@gmail.com>
2023-06-13 12:21:48 +0900

    a

---
+++ Plantiful-f/src/main/java/com/example/demo/PlantifulFApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantifulFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantifulFApplication.class, args);
+	}
+
+}
