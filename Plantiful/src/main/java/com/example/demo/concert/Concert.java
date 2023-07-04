f6945bfbde77d4bdcbc0b6e7b87512ae4e3b1b67
Juwon <KOSTA@DESKTOP-307CT33>
2023-07-04 17:31:35 +0900

    Concert 클래스 수정, ConcertController detail 기능 추가

--- Plantiful/src/main/java/com/example/demo/concert/Concert.java
+++ Plantiful/src/main/java/com/example/demo/concert/Concert.java
@@ -13,16 +13,32 @@ public class Concert {
 	private String id;
 	private String name; 
 	private String poster;
-	private String[] styurl;
+	private String[] styurls;
 	private String startDate; 
 	private String endDate; 
-	private String loc; 
 	private String cast;
 	private String crew; 
 	private String runTime; 
 	private String age; 
+	private String loc; 
 	private String producer;
 	private String price; 
+	private String sty;
+	private String genre;
 	private String state; 
 	private boolean openrun;
+	private String locId;
+	private String concertTime;
+
+	public Concert(String id, String name, String poster, String startDate, String endDate, String loc, String genre, 
+			String state, boolean openrun) {
+		this.id = id;
+		this.name = name;
+		this.poster = poster;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.loc = loc;
+		this.state = state;
+		this.openrun = openrun;
+	}
 }
--- Plantiful/src/main/java/com/example/demo/concert/ConcertController.java
+++ Plantiful/src/main/java/com/example/demo/concert/ConcertController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -29,24 +30,22 @@ import org.xml.sax.SAXException;
 @RequestMapping("/concert")
 public class ConcertController {
 	
-	@GetMapping("")
-	public Map findAll(int page) {
-		System.out.println("findAll");
-		System.out.println("page : "+page);
+	@GetMapping("/{page}")
+	public Map findAll(@PathVariable(name = "page") int page) {
         LocalDate now = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         String formatedNow = now.format(formatter);
         LocalDate plusMonth = now.plusMonths(1);
 		String formatedPlusMonth = plusMonth.format(formatter);
 		ArrayList<Concert> list = new ArrayList<>();
-		
 		String str = "http://kopis.or.kr/openApi/restful/pblprfr";
 		str += "?service=ce866e84481d4d8cb7883e90889e4a9d";
 		str += "&stdate="+formatedNow;
 		str += "&eddate="+formatedPlusMonth;
 		str += "&cpage="+page;
-		str += "&rows=10";
-		
+		str += "&rows=2000";
+		str += "&prfstate=01";
+		str += "&prfstate=02";
 		try {
 			URL url = new URL(str);
 			URLConnection connect = url.openConnection();
@@ -57,9 +56,22 @@ public class ConcertController {
 			NodeList nodeList = root.getElementsByTagName("db");
 			for(int i=0; i<nodeList.getLength(); i++) {
 				Element element = (Element) nodeList.item(i);
+				String id = element.getElementsByTagName("mt20id").item(0).getTextContent();
+				String name = element.getElementsByTagName("prfnm").item(0).getTextContent();
+				String poster = element.getElementsByTagName("poster").item(0).getTextContent();
+				String startDate = element.getElementsByTagName("prfpdfrom").item(0).getTextContent();
+				String endDate = element.getElementsByTagName("prfpdto").item(0).getTextContent();
+				String loc = element.getElementsByTagName("fcltynm").item(0).getTextContent();
+				String genre = element.getElementsByTagName("genrenm").item(0).getTextContent();
+				String state = element.getElementsByTagName("prfstate").item(0).getTextContent();
+				String strOpenrun = element.getElementsByTagName("openrun").item(0).getTextContent();
+				boolean openrun = false;
+				if(strOpenrun.equals("Y")) {
+					openrun = true;
+				} 
 				
-//				Concert con = new Con
-//				list.add();
+				Concert concert = new Concert(id, name, poster, startDate, endDate, loc, genre, state, openrun);
+				list.add(concert);
 			}
 			
 		} catch (MalformedURLException e) {
@@ -74,6 +86,44 @@ public class ConcertController {
 		
 		Map map = new HashMap();
 		map.put("list", list);
+		return map;
+	}
+	
+	@GetMapping("/detail/{id}")
+	public Map getDetail(@PathVariable(name = "id") String id) {
+		String str = "http://kopis.or.kr/openApi/restful/pblprfr/"+id;
+		str += "?service=ce866e84481d4d8cb7883e90889e4a9d";
+		try {
+			URL url = new URL(str);
+			URLConnection connect = url.openConnection();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = (Document) builder.parse(connect.getInputStream());
+			Element root = doc.getDocumentElement();
+			NodeList nodeList = root.getElementsByTagName("db");
+			Element element = (Element) nodeList.item(0); 
+			String mtid = element.getElementsByTagName("mt20id").item(0).getTextContent();
+			String name = element.getElementsByTagName("prfnm").item(0).getTextContent();
+			String poster = element.getElementsByTagName("poster").item(0).getTextContent();
+			System.out.println(element.getElementsByTagName("poster").getLength());
+			Node node = element.getElementsByTagName("styurls").item(0);
+			System.out.println(node);
+ 			String startDate = element.getElementsByTagName("prfpdfrom").item(0).getTextContent();
+			String endDate = element.getElementsByTagName("prfpdto").item(0).getTextContent();
+			
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		
+		
+		Map map = new HashMap();
 		return map;
 	}
 }
