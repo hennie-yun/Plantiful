9b695d41cba81704de712add12942625eb6e5f2a
SkylarLee1003 <skylarlee1003@gmail.com>
2023-07-07 16:17:01 +0900

    환불 오류 해결

--- Plantiful/src/main/java/com/example/demo/subscribeparty/SubscribePartyController.java
+++ Plantiful/src/main/java/com/example/demo/subscribeparty/SubscribePartyController.java
@@ -102,7 +102,7 @@ public class SubscribePartyController {
 	
 
 	//돈 움직이기(팀원들 pb 0으로 만듦)
-	@PatchMapping("/money/{subscribe_num}")
+	@PostMapping("/money/{subscribe_num}")
 	public void minusMoney(@PathVariable("subscribe_num") int subscribe_num) {
 //	    try {
 	        service.minusMoney(subscribe_num);
