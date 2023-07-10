30f9d2f6cc67059d52bb69c82cfa064495e95598
rmsgml7553 <rmsgml7553@naver.com>
2023-07-10 12:33:23 +0900

    naver 회원가입 완료!!

--- Plantiful/src/main/java/com/example/demo/naver/NaverTokenVo.java
+++ Plantiful/src/main/java/com/example/demo/naver/NaverTokenVo.java
@@ -1,8 +1,11 @@
 package com.example.demo.naver;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NaverTokenVo {
 	private String access_token;
 	private String refresh_token;
